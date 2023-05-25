37ebaccc65a0c907dc224b1c19751dc6205890f8
Ikumi Mine <ikumine@gmail.com>
2023-05-25 10:42:36 -0400

    loading

--- src/Components/Loading/index.jsx
+++ src/Components/Loading/index.jsx
@@ -1,8 +1,21 @@
 import './Loading.css';
+import video from '../../Assets/travelvideo.mp4'
 
 const Loading = () => {
     return (
-        <div>This is loading page</div>
+        <div className="video-container">
+            <svg viewBox="0 0 100 100">
+                <defs>
+                    <clipPath id="videoText">
+                        <text>Rivera Travel</text>
+                    </clipPath>
+                </defs>
+            </svg>
+
+            <video loop autoPlay muted>
+                <source src={video} type="video/mp4"/>
+            </video>
+        </div>
     )
 }
 
