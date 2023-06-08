3addf361225a7952bb10c7532306c0069cb7a38a
Ikumi Mine <ikumine@gmail.com>
2023-06-08 14:01:15 -0400

    loading responsive v0.1

--- src/Components/Loading/index.jsx
+++ src/Components/Loading/index.jsx
@@ -1,16 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import './Loading.css';
 import video from '../../Assets/travelvideo.mp4'
 
 const Loading = () => {
+
+    const [screenWidth, setScreenWidth] = useState(0);
+
+    const getCurrentWidth = () => {
+        setScreenWidth(window.innerWidth);
+    }
+
+    useEffect(()=> {
+        window.addEventListener('resize', getCurrentWidth);
+    }, [screenWidth]);
+    
     return (
         <div className="video-container">
-            <svg viewBox="0 0 100 100">
-                <defs>
-                    <clipPath id="videoText">
-                        <text>Rivera Travel</text>
-                    </clipPath>
-                </defs>
-            </svg>
+            <div className="svg-container">
+                <svg
+                viewBox="0 0 1300 250"
+                xmlns="http://www.w3.org/2000/svg">
+                    <defs>
+                        <clipPath id="videoText" clipPathUnits="objectBoundingBox">
+                            {screenWidth > 500 
+                                ? <text x="15%" y="98%" textLength="100%">Rivera Travel</text>
+                                : <>
+                                    <text x="40%" y="80%" textLength="50%">Rivera</text>
+                                    <text x="40%" y="120%" textLength="50%">Travel</text>
+                                  </>
+                            }
+                        </clipPath>
+                    </defs>
+                </svg>
+            </div>
 
             <video loop autoPlay muted>
                 <source src={video} type="video/mp4"/>
